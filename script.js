36a0a2b31ec78521e84cbb892d17d28db3d80a27
NwinNwin <ndnguyen2003@gmail.com>
2023-10-18 17:12:56 -0700

    worked on about page + portfolio + mobile view

---
+++ script.js
@@ -0,0 +1,2 @@
+// alert("BOB the builder")
+// window.location.href = "https://www.google.com/search?q=rick+roo&rlz=1C1CHBF_enUS1055US1056&oq=rick+roo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIPCAEQABgKGIMBGLEDGIAEMg8IAhAAGAoYgwEYsQMYgAQyDAgDEC4YChixAxiABDIHCAQQLhiABDIJCAUQLhgKGIAEMg8IBhAAGAoYgwEYsQMYgAQyCQgHEAAYChiABDINCAgQLhivARjHARiABDIHCAkQABiABNIBCDE1ODBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8";
