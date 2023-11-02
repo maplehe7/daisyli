87fbe9e980cbabe4a62a424e837dfdd2e81eb2ca
Ethan He <maplehe7@gmail.com>
2023-11-01 17:12:36 -0700

    Repl Import

--- script.js
+++
@@ -1,2 +0,0 @@
-// alert("BOB the builder")
-// window.location.href = "https://www.google.com/search?q=rick+roo&rlz=1C1CHBF_enUS1055US1056&oq=rick+roo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIPCAEQABgKGIMBGLEDGIAEMg8IAhAAGAoYgwEYsQMYgAQyDAgDEC4YChixAxiABDIHCAQQLhiABDIJCAUQLhgKGIAEMg8IBhAAGAoYgwEYsQMYgAQyCQgHEAAYChiABDINCAgQLhivARjHARiABDIHCAkQABiABNIBCDE1ODBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8";
