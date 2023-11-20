327b23dc759e056a6b839097c7d637cfd35f583f
maplehe7 <144498852+maplehe7@users.noreply.github.com>
2023-11-20 13:52:08 -0800

    Updated Featured Videos
    Co-Authored-By: Dang Nguyen Nguyen <93059380+NwinNwin@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-  
+var videoList = [
+  "https://player.vimeo.com/video/584055906?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479",
+  "https://player.vimeo.com/video/564693956?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479",
+  "https://player.vimeo.com/video/549409748?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479",
+  "https://player.vimeo.com/video/522138099?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479"
+];
+
+// Index to keep track of the current video
+var currentIndex = 0;
+
+function changeVideo(direction) {
+  if (direction === 'prev') {
+    currentIndex = (currentIndex - 1 + videoList.length) % videoList.length;
+  } else if (direction === 'next') {
+    currentIndex = (currentIndex + 1) % videoList.length;
+  }
+
+  // Update the src attribute of the iframe
+  document.getElementById('webpage').src = videoList[currentIndex];
 }
-console.log('%cThis website was made by Ethan He. You can support him by donating to his gofundme. He has permanent leg and brain damage and will have died by the time you are reading this.', 'color: green; font-size: 20px; font-weight:bold;');
